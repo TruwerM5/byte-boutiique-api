0620d9950e78b18e50deddb9f14aefa525d869f2
Raaydan <ilsur2000.2016@gmail.com>
2024-03-07 23:39:42 +0300

    create first product, add route for products/:category, set up prisma service

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { ProductsService } from './products.service';
+
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+    constructor(private productsService: ProductsService) {}
+
+    @Get(":category")
+    async getByCategory(@Param() params): Promise<any> {
+        return this.productsService.getByCategory(params.category);
+    }
+}
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
-
+import { PrismaService } from 'src/prisma.service';
+import { Product, Prisma } from '@prisma/client';
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+    constructor(private prisma: PrismaService) {}
+
+    async getByCategory(category: string): Promise<any> {
+        return this.prisma.product.findMany({
+            where: {
+                category,
+            }
+        })
+    }
+}
